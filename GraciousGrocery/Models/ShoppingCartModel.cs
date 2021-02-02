13d0f3d4f8b092c73794c3cd91b93c3f4918b40a
vasicvanja <48738688+vasicvanja@users.noreply.github.com>
2021-02-02 16:24:11 +0100

    ShoppingCart

---
+++ GraciousGrocery/Models/ShoppingCartModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GraciousGrocery.Models
+{
+    public class ShoppingCartModel
+    {
+        [Key]
+        public string UserId { get; set; }
+        public List<int> Quantity { get; set; }
+        public virtual List<Product> products { get; set; }
+
+        public ShoppingCartModel(string userID)
+        {
+            Quantity = new List<int>();
+            products = new List<Product>();
+            UserId = userID;
+        }
+
+        public ShoppingCartModel()
+        {
+            Quantity = new List<int>();
+            products = new List<Product>();
+        }
+    }
+}
---
+++ GraciousGrocery/Models/SubmitedProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GraciousGrocery.Models
+{
+    public class SubmitedProducts
+    {
+        [Key]
+        public string UserId { get; set; }
+        public virtual List<Product> Products { get; set; }
+        public SubmitedProducts()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
